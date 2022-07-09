a62f9f8074ac04f85e96480b54d9ef0e26ccb641
SHIVSB <2020kuec2005@iiitkota.ac.in>
2022-07-09 18:42:37 +0530

    adding jest for unit testing

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  roots: ["<rootDir>/test/"],
+  testMatch: ["**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testTimeout: 60000,
+};
