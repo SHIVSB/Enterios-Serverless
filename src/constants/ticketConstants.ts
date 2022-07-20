74585c1ff6b8228fdcd255a94566f4a1d7147e52
SHIVSB <2020kuec2005@iiitkota.ac.in>
2022-07-20 18:45:25 +0530

    created ticket model && added notification service client code

---
+++ src/constants/ticketConstants.ts
@@ -0,0 +1,14 @@
+export const ticketConstants = {
+    ticketStatus: {
+        open: "OPEN",
+        closed: "CLOSED",
+        blocked: "BLOCKED",
+        inProgress: "IN_PROGRESS"
+    },
+    ticketPriority: {
+        one: 1,
+        two: 2,
+        three: 3,
+        four: 4
+    }
+}
---
+++ src/models/ticket.model.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+const ticketConstants = require("../constants/ticketConstants")
+
+export type ticketDocument = mongoose.Document & {
+    title: string;
+    description: string;
+    ticketPriority: number;
+    status: string;
+    reporter: string;
+    assignee: string;
+};
+
+const ticketSchema = new mongoose.Schema<ticketDocument>(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        ticketPriority: {
+            type: Number,
+            required: true,
+            default: ticketConstants.ticketPriority.four  // possilbe values 1/2/3/4
+        },
+        status: {
+            type: String,
+            required: true,
+            default: ticketConstants.ticketStatus.open  // possible values OPEN CLOSED BLOCKED
+        },
+        reporter: {
+            type: String
+        },
+        assignee: {
+            type: String
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Ticket = mongoose.model<ticketDocument>(
+    "Ticket",
+    ticketSchema
+);
